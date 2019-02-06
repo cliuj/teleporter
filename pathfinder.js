6c74d6a15d8d32388ff0bc24ecd864876703e0f9
jliu2179 <jliu2179@gmail.com>
2019-02-05 23:57:34 -0500

    add -l option

--- pathfinder.js
+++ pathfinder.js
@@ -5,3 +5,34 @@ const { promisify } = require('util');
 const readline = require('readline');
 const db = require('./db');
 const args = process.argv.slice(2);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.close();
+
+
+async function listDBContents() {
+  await db.createReadStream()
+    .on('data', function (data) {
+      console.log(data.key, '=', data.value)
+    })
+    .on('error', function (err) {
+      console.log('Oh my!', err)
+    })
+}
+
+
+function processArgs() {
+  switch(args[0]) {
+    case '-l': case '--list':
+      listDBContents();
+      break;
+    default:
+      console.log("default reached");
+  }
+}
+
+processArgs();
