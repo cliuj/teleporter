97e766875b008f4287b275071072f4b1ff40a76e
jliu2179 <jliu2179@gmail.com>
2019-02-06 22:42:56 -0500

    This commit includes the following:
    * add key-value to DB function - adds a function that maps a passed
        key --> value
    
    * an cli option to use key-value function - allows users to map a
        key --> value and store it into the DB through the CLI;
        the value is meant to be the dir path which will be
        implemented in the future
    
    * a confirmation function - asks for user confirmation to continue with
        action

--- pathfinder.js
+++ pathfinder.js
@@ -12,7 +12,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.close();
 
 async function checkToggled() {
   await db.get('toggled', function (err, value) {
@@ -43,9 +42,53 @@ async function toggleConfirms() {
   }
 }
 
+async function confirm() {
+  let answer = false;
+  rl.question[promisify.custom] = (arg) => {
+    return new Promise((resolve) => {
+      rl.question(arg, resolve);
+    });
+  };
+  
+  const ask = promisify(rl.question);
+  await ask("Do you wish to continue? [y/N]: ")
+    .then(ans => {
+      if (ans.toLowerCase() === 'y') {
+        answer = true;
+      } else {
+        answer = false;
+      }
+      rl.close();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  return answer;
+}
+
+async function addDBLocation(key, path) {
+  if (key === "toggled") {
+    return console.log("Key: 'toggled' is a keyword!' It cannot be used.");
+  }
+  
+  // check if the key exists
+  await db.get(key, function(err) {
+    if (err) {
+      if (err.notFound) {
+        return db.put(key, path);
+      }
+    }
+  });
+
+  console.log("Key already in-use. Continuing will overwrite the value of the key.");
+  if (await confirm()) {
+    return db.put(key, path);
+  }
+}
+
 async function deleteDBLocation(key) {
   if (key === "toggled") {
-    return console.log("'" + key + "'", "is a keyword, so it cannot be deleted!");
+    return console.log("'" + key + "'", "is a keyword! It cannot be deleted!");
   }
 
   await db.del(key, function(err) {
@@ -56,7 +99,6 @@ async function deleteDBLocation(key) {
     }
   });
 }
-
 
 async function listDBContents() {
   await db.createReadStream()
@@ -73,20 +115,33 @@ function displayHelp() {
 }
 
 function processArgs() {
+  if(debug) {
+    console.log(args);
+  }
+
   switch(args[0]) {
     case '-tc': case '--toggle-confirm':
+      rl.close();
       return toggleConfirms();
+
+    case '-a': case '--add':
+      
+      return addDBLocation(args[1], args[2]);
 
     case '-d': case '--delete':
       if (!args[1]) {
+        rl.close();
         return console.log("No args passed");
       }
+      rl.close();
       return deleteDBLocation(args[1]);
 
     case '-l': case '--list':
+      rl.close();
       return listDBContents();
     
     case '-h': case '--help':
+      rl.close();
       return displayHelp();
 
     default:
